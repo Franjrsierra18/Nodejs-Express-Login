0798bd1595efe2010d8b1f0fb555cee83f9defbd
Francisco Sierra <franjrsierra18@gmail.com>
2019-09-15 18:46:23 +0200

    fixed the connection with mongoDB

--- index.js
+++ index.js
@@ -1,50 +1,47 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 // Connect to DB
-mongoose.connect('mongodb://localhost:27017/db_Node', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb+srv://franjrsierra18:Sierra2991@registro-travel-agency-bhwrc.mongodb.net/db_Node?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
 let db = mongoose.connection;
+
+//Check connection
+db.once('open', () => {
+  console.log('Connected to MongoDB...');
+})
+
+// Check for DB errors
+db.on('error', (error) => {
+  console.log(error);
+})
 
 // Init App
 const app = express();
+
+//Bring in Models
+const Article = require('./models/Article');
 
 // Load View Engine
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
+
 // Home Route
 app.get('/', (req,res) => {
-  let articles = [
-    {
-      id: 1,
-      title: 'article one',
-      author: 'Frank',
-      body: 'this is article one'
-    },
-    {
-      id: 2,
-      title: 'article two',
-      author: 'Jose',
-      body: 'this is article two'
-    },
-    {
-      id: 3,
-      title: 'article three',
-      author: 'Luis',
-      body: 'this is article three'
-    },
-    {
-      id: 4,
-      title: 'article four',
-      author: 'Juan',
-      body: 'this is article four'
-    },
-  ]
-  res.render('index', {
-    title: 'Home',
-    articles
-  });
+  Article.find({}, (err, articles) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render('index', {
+        title: 'Home',
+        articles: articles
+      });
+    }
+  })
 });
 
 app.get('/articles/add', (req,res) => {
@@ -52,6 +49,21 @@ app.get('/articles/add', (req,res) => {
     title: 'Add Articles'
   });
 });
+
+app.post('/articles/add', (req,res) => {
+  const article = new Article();
+  article.title = req.body.title;
+  article.author = req.body.author;
+  article.body = req.body.body;
+  article.save((err) => {
+    if (err) {
+      console.log(err);
+      return;
+    } else {
+      res.redirect('/');
+    }
+  })
+})
 
 // Start Server
 app.listen(3000, () => console.log('Server started on port 3000'));
